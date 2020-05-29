66718dc778641add776c5e4eede6a714aa88f6a3
Kaleab G <kaleabsolomon470@gmail.com>
2020-05-28 22:52:30 -0500

    Can read credentials from local file & open links from file

--- Linkedin.py
+++ Linkedin.py
@@ -3,11 +3,18 @@ from getpass import getpass
 
 browser = webdriver.Chrome()
 
+FILE_NAME = "linkedin_contacts.txt"
 
 browser.get("https://www.linkedin.com/login")
 
-username = input("Enter in your linkedin username: ")
-password = getpass("Enter your password: ")
+#username = input("Enter in your linkedin username: ")
+#password = getpass("Enter your password: ")
+file = open("credentials.txt", "r")
+allLines = file.readlines()
+
+username = allLines[0].strip()
+password = allLines[1].strip()
+
 
 usernameTextbox = browser.find_element_by_id("username")
 usernameTextbox.send_keys(username)
@@ -18,5 +25,15 @@ passwordTextbox.send_keys(password)
 signInButton = browser.find_element_by_tag_name("button")
 signInButton.click()
 
-# links will go here
-browser.get("https://www.linkedin.com/in/kaleab-gebremichael")
+# open file containing links
+file = open(FILE_NAME, "r")
+allLinks = file.readlines()
+link = ""   #initialize it as string to use the string methods
+
+for link in allLinks:
+    trimmedLink = link.strip()
+    sendInvite(trimmedLink)
+
+def sendInvite(userLink):
+    # browser.get("https://www.linkedin.com/in/kaleab-gebremichael")
+    browser.get(userLink)
