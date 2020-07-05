ed08062cf3f3a5bccd69d28a08608bd20f30471f
Kaleab G <kaleabsolomon470@gmail.com>
2020-07-04 22:59:51 -0500

    First working version

--- Linkedin.py
+++ Linkedin.py
@@ -3,6 +3,7 @@ from getpass import getpass
 
 browser = webdriver.Chrome()
 
+# file containing all linkedin links
 FILE_NAME = "linkedin_contacts.txt"
 
 browser.get("https://www.linkedin.com/login")
@@ -30,10 +31,19 @@ file = open(FILE_NAME, "r")
 allLinks = file.readlines()
 link = ""   #initialize it as string to use the string methods
 
+def sendInvite(userLink):
+    browser.get(userLink)
+    connectButtonXpathLink = "/html/body/div[7]/div[3]/div/div/div/div/div[2]/main/div[1]/section/div[2]/div[1]/div[2]/div/div/span[1]/div/button"
+    connectKey = browser.find_element_by_xpath(connectButtonXpathLink)
+    connectKey.click();
+
+    doneButtonXpathLink = "/html/body/div[4]/div/div/div[3]/button[2]"
+    doneKey = browser.find_element_by_xpath(doneButtonXpathLink)
+    doneKey.click();
+
+
 for link in allLinks:
     trimmedLink = link.strip()
     sendInvite(trimmedLink)
 
-def sendInvite(userLink):
-    # browser.get("https://www.linkedin.com/in/kaleab-gebremichael")
-    browser.get(userLink)
+
